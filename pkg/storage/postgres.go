1b1cb4a86e536a0101e7259c5b4565bf81e01edd
Daniil <dan.ponyavin@yandex.ru>
2024-08-18 22:17:16 +0300

    Created Dockerfile and docker-compose

--- pkg/storage/postgres.go
+++ pkg/storage/postgres.go
@@ -2,7 +2,9 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 type PostgresStorage struct {
@@ -10,7 +12,15 @@ type PostgresStorage struct {
 }
 
 func NewPostgresStorage() *PostgresStorage {
-	connStr := "host=localhost port=5436 user=postgres password=mypass dbname=test sslmode=disable"
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5436"
+	}
+	connStr := fmt.Sprintf("host=%s port=%s user=postgres password=mypass dbname=test sslmode=disable", host, port)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
